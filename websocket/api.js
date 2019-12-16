1aa9017335f727dda5e3575052703597b984ca6f
numanayhan <numanayhan1903@gmail.com>
2019-12-16 15:57:16 +0300

    add server and client

--- websocket/api.js
+++ websocket/api.js
@@ -1,30 +1,15 @@
 const http = require("http");
-const WebSocket = require("ws");
-const url = require("url");
+ 
+const WebSocket = require('ws')
+ 
+const wss = new WebSocket.Server({ port: 8080 })
+ 
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('Hello! Message From Server!!')
+}) ://localhost:8081");
 
-const server = http.createServer();
-const wss1 = new WebSocket.Server({ noServer: true });
-const wss2 = new WebSocket.Server({ noServer: true });
-
-const ws = new WebSocket("wss://echo.websocket.org/", {
-  origin: "https://websocket.org"
-});
-
-ws.on("open", function open() {
-  console.log("connected");
-  ws.send(Date.now());
-});
-
-ws.on("close", function close() {
-  console.log("disconnected");
-});
-
-ws.on("message", function incoming(data) {
-  console.log(`Roundtrip time: ${Date.now() - data} ms`);
-
-  setTimeout(function timeout() {
-    ws.send(Date.now());
-  }, 500);
-});
 console.log("listen : 8080");
 server.listen(8080);
