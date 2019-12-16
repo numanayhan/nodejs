2a77f510d063ca2bc9fa5c9f1f7ddc21e15f0097
numanayhan <numanayhan1903@gmail.com>
2019-12-16 18:06:55 +0300

    fixed

--- websocket/api.js
+++ websocket/api.js
@@ -1,15 +1,15 @@
 const http = require("http");
- 
-const WebSocket = require('ws')
- 
-const wss = new WebSocket.Server({ port: 8080 })
- 
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-  })
-  ws.send('Hello! Message From Server!!')
-}) ://localhost:8081");
+
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", ws => {
+  ws.on("message", message => {
+    console.log(`Received message => ${message}`);
+  });
+  ws.send("Hello! Message From Server!!");
+});
 
 console.log("listen : 8080");
 server.listen(8080);
